71710e0422e0df043e9515be48d47458de4e38b9
HanifWadowskyy <43465633+HanifWadowskyy@users.noreply.github.com>
2022-03-11 15:28:45 +0000

    Update VersionInfo.php

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -34,5 +34,5 @@ const _VERSION_INFO_INCLUDED = true;
 
 const NAME = "ElyNetBE";
 const BASE_VERSION = "3.30.0";
-const IS_DEVELOPMENT_BUILD = true;
-const BUILD_CHANNEL = "master";
+const IS_DEVELOPMENT_BUILD = false;
+const BUILD_CHANNEL = "stable";
